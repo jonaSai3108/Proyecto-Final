3627fd5379f4deb19d0558b105c23d5d54954b42
Evichs <elisonvail@umes.edu.gt>
2025-06-09 16:31:23 -0600

    Prueba de resultado

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ def get_connection():
             host='localhost',         
             port=3306,                
             user='root',       
-            password='12345678', 
+            password='Elison21!', 
             database='DBTorneosFutbol'
         )
         if connection.is_connected():
